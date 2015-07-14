1b39baacf2183c61b2a872c2b52801eceb1a6491
marcelinol <marcelinolucianom@gmail.com>
2015-07-14 18:29:51 -0300

    improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :historical_quotes_loader
+  root 'historical_quotes_loader#index'
+
+  get 'historical_quotes_loader/index'
+
+  post 'historical_quotes_loader/load'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
